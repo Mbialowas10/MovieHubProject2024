a8ae4d56eadeb6c93952cd10a8795e11da18a049
Mike bialowas <mikebialowas@Mikes-MBP.wp.shawcable.net>
2024-09-26 09:35:05 -0500

    Added Moshi Library to project_279

---
+++ app/src/main/java/com/mbialowas/moviehubproject2024/api/model/Movie.kt
@@ -0,0 +1,17 @@
+package com.mbialowas.moviehubproject2024.api.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Movie(
+    @Json(name = "page")
+    val page: Int,
+    @Json(name = "results")
+    val results: List<Result>,
+    @Json(name = "total_pages")
+    val totalPages: Int,
+    @Json(name = "total_results")
+    val totalResults: Int
+)
