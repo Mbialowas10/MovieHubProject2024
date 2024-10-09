a23103f493fd6a7624e41266cec6c2dd7c4fe80a
Michael Bialowas <mbialowas@rrc.ca>
2024-10-09 08:53:33 -0500

    SearchFeature Screen Draft_s279

--- app/src/main/java/com/mbialowas/moviehubproject2024/screens/SearchScreen.kt
+++ app/src/main/java/com/mbialowas/moviehubproject2024/screens/SearchScreen.kt
@@ -2,27 +2,59 @@ package com.mbialowas.moviehubproject2024.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun SearchScreen(){
+    val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    var query by rememberSaveable { mutableStateOf("") } // come back to this and viewmodel
+
+
     Box(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.Cyan)
+        .background(color = Color.LightGray)
     ){
-        Text(
-            color = Color.White,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.align(Alignment.Center),
-            text = "Search Screen"
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                modifier = Modifier
+                    .background(color = Color.Black)
+                    .fillMaxWidth(),
+                text="Search Screen",
+                color = Color.White,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+//            OutlinedTextField(
+//                value "Hi Class",
+//                onValueChange = {
+//                    query = it
+//                }
+//
+//            )
+        }
+
     }
 }
